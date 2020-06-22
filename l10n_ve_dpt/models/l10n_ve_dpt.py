65501d4d1e967fea83de9321cb2385073b2765e7
Francisco Lercari <franciscolercari@gmail.com>
2020-06-22 13:43:29 -0400

    [FIX] test travis flake8

--- l10n_ve_dpt/models/l10n_ve_dpt.py
+++ l10n_ve_dpt/models/l10n_ve_dpt.py
@@ -7,7 +7,7 @@ class CountryState(models.Model):
     """ Add Municipalities reference in State """
     _name = "res.country.state"
     _inherit = "res.country.state"
-    _description= "Country states"
+    _description = "Country states"
 
     municipality_id = fields.One2many(
         "res.country.state.municipality", "state_id", "Municipalities in this state"
@@ -18,13 +18,13 @@ class CountryState(models.Model):
 class StateMunicipality(models.Model):
     """States Municipalities"""
     _name = "res.country.state.municipality"
-    _description= "State municipalities"
+    _description = "State municipalities"
 
     state_id = fields.Many2one(
         "res.country.state",
         "State",
         required=True,
-        help="Name of the State to which the municipality belongs"
+        help="Name of the State to which the municipality belongs",
     )
     name = fields.Char("Municipality", required=True, help="Municipality name")
     parish_id = fields.One2many(
@@ -37,7 +37,7 @@ class StateMunicipality(models.Model):
 class MunicipalityParish(models.Model):
     """States Parishes"""
     _name = "res.country.state.municipality.parish"
-    _description= "Municipality parishes"
+    _description = "Municipality parishes"
 
     municipality_id = fields.Many2one(
         "res.country.state.municipality",
@@ -46,3 +46,4 @@ class MunicipalityParish(models.Model):
     )
     name = fields.Char("Parish", required=True, help="Parish name")
     ubigeo = fields.Char(string="ubigeo code", size=6)
+    
