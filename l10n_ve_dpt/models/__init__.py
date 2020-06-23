1883f1eaa5b3205407477899bb47d46eef403cdb
Francisco Lercari <franciscolercari@gmail.com>
2020-06-23 07:19:41 -0400

    [FIX] reformat

--- l10n_ve_dpt/models/__init__.py
+++ l10n_ve_dpt/models/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).nerated by the Odoo plugin for Dia !
 
-from . import l10n_ve_dpt
-from . import res_partner
+from . import l10n_ve_dpt, res_partner
